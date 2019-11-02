e9bcf44eb00ad4206aacd7e83657fe604e5fc3b8
gcascade <geoffroy.cascade@reseau.eseo.fr>
2019-11-02 20:47:08 +0100

    Before ejection

---
+++ Navigation/Navigation.js
@@ -0,0 +1,77 @@
+// Navigation/Navigation.js
+
+import React from 'react'
+import { StyleSheet, Image } from 'react-native'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetail'
+import Favorites from '../Components/Favorites'
+import Test from '../Components/Test'
+
+const SearchStackNavigator = createStackNavigator({
+  Search: { // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
+    screen: Search,
+    navigationOptions: {
+      title: 'Rechercher'
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+})
+
+const FavoritesStackNavigator = createStackNavigator({
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      title: 'Favoris'
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+})
+
+const MoviesTabNavigator = createBottomTabNavigator({
+  Test: {screen : Test},
+  Search: {
+    screen: SearchStackNavigator,
+    navigationOptions: {
+      tabBarIcon: () => {
+        return <Image 
+          source={require('../Images/ic_search.png')}
+          style={styles.icon}/>
+      }
+    }
+  },
+  Favorites: {
+    screen: FavoritesStackNavigator,
+    navigationOptions: {
+      tabBarIcon: () => {
+        return <Image 
+        source={require('../Images/ic_favorite.png')}
+        style={styles.icon}/>
+      }
+    }
+  }
+},
+  {
+    tabBarOptions : {
+      activeBackgroundColor: '#DDDDDD',
+      inactiveBackgroundColor: '#FFFFFF',
+      showLabel: false,
+      showIcon: true
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
